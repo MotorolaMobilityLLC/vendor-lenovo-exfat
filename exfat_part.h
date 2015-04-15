e6c33b21ec4645c2d2ac7565d74876cb8f9500b7
wangxf14 <wangxf14@lenovo.com>
2015-04-15 17:27:07 +0800

    [VIBEUI_L_DEV][VIBEUIDEVL-2207] 去掉samsung的标识，在exfat_core模块中根据开源代码注释在源代码上添加注释

--- exfat_part.h
+++ exfat_part.h
@@ -1,5 +1,4 @@
 /*
- *  Copyright (C) 2012-2013 Samsung Electronics Co., Ltd.
  *
  *  This program is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU General Public License
--- exfat_super.h
+++ exfat_super.h
@@ -1,7 +1,6 @@
 /* Some of the source code in this file came from "linux/fs/fat/fat.h".  */
 
 /*
- *  Copyright (C) 2012-2013 Samsung Electronics Co., Ltd.
  *
  *  This program is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU General Public License
